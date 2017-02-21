ffe308ab22a82ccef7d36a00905094bc7961fadb
butyh <butyh1@gmail.com>
2017-02-21 22:09:37 +0300

    fixed video removal

--- cleanup_video.sh
+++ cleanup_video.sh
@@ -10,8 +10,7 @@ do
   USED_PERCENT=`df -h --output=pcent / | sed -e '1d' | sed -e 's/ //' | sed -e 's/%//'`
   while [ $USED_PERCENT -gt 10 ]
   do
-      FNAME=`ls -tr $VIDEO_PATH/20* | head -n 1`
-      rm /$VIDEO_PATH/$FNAME
+      rm `ls -tr $VIDEO_PATH/20* | head -n 1`
       USED_PERCENT=`df -h --output=pcent / | sed -e '1d' | sed -e 's/ //' | sed -e 's/%//'`
   done
   sleep 5m
