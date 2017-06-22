41bbb848dcc208f04036b473579eb699385a1682
butyh <butyh1@gmail.com>
2017-06-22 21:24:04 +0300

    added supervisor configs

--- motion_detect.sh
+++ motion_detect.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ffmpeg -i "rtsp://192.168.0.31:554/12" -threads 4 -filter:v "select='gt(scene,0.05)',showinfo" -vsync 0 frames/%05d.jpg
+
+ffmpeg -i "rtsp://$1:554/12" -threads 4 -filter:v "select='gt(scene,0.05)',showinfo" -vsync 0 $2/%05d.jpg
